2377e518e38f56cf61ab7096e57429fb33aeb548
Caoimhinmg <kevin.gaastra@gmail.com>
2018-01-18 12:51:41 -0600

    modified make_tab to read both aero and ship at the same time when run in your top level work directory and leave a header to make outfiles much easier to read into geomapapp, still needs work to make more usable

--- pyskew/make_tab.py
+++ pyskew/make_tab.py
@@ -3,9 +3,14 @@ import pandas as pd
 
 directory = sys.argv[1]
 
-cut_tracks = glob.glob(os.path.join(directory,'**','**','**','*.c?'))
+ship_cut_tracks = glob.glob(os.path.join(directory,'**','**','**','*.c?'))
+aero_cut_tracks = glob.glob(os.path.join(directory,'**','**','**','**','*.c?'))
 print(cut_tracks)
 
-for cut_track in cut_tracks:
-    df = pd.read_csv(cut_track,sep=' ')
-    df.to_csv(cut_track.split('.')[0]+'_tab.'+cut_track.split('.')[1],sep='\t',header=False,index=False)
+for cut_track in ship_cut_tracks:
+    df = pd.read_csv(cut_track,sep=' ',names=['dis','dec_year','mag','lat','lon'])
+    df.to_csv(cut_track.split('.')[0]+'_tab.'+cut_track.split('.')[1],sep='\t',index=False)
+
+for cut_track in aero_cut_tracks:
+    df = pd.read_csv(cut_track,sep=' ',names=['dis','lat','lon','u1','u2','u3','u4','u5','u6','u7','u8','u9'])
+    df.to_csv(cut_track.split('.')[0]+'_tab.'+cut_track.split('.')[1],sep='\t',index=False)
